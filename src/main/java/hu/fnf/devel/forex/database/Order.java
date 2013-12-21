f96aa57d92709543635f9df3cc73994f1442f047
csikosbalint <csikos.balint@fnf.hu>
2013-12-21 19:57:20 +0100

    #20 and the files...

---
+++ src/main/java/hu/fnf/devel/forex/database/Order.java
@@ -0,0 +1,108 @@
+package hu.fnf.devel.forex.database;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the Orders database table.
+ * 
+ */
+@Entity
+@Table(name="Orders")
+@NamedQuery(name="Order.findAll", query="SELECT o FROM Order o")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(insertable=false, updatable=false)
+	private int id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date close;
+
+	private String laststate;
+
+	private int orderid;
+
+	private String period;
+
+	private float profit;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date start;
+
+	//bi-directional many-to-one association to Strategy
+	@ManyToOne
+	@JoinColumn(name="strategyid")
+	private Strategy strategy;
+
+	public Order() {
+	}
+
+	private int getId() {
+		return this.id;
+	}
+
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getClose() {
+		return this.close;
+	}
+
+	public void setClose(Date close) {
+		this.close = close;
+	}
+
+	public String getLaststate() {
+		return this.laststate;
+	}
+
+	public void setLaststate(String laststate) {
+		this.laststate = laststate;
+	}
+
+	public int getOrderid() {
+		return this.orderid;
+	}
+
+	public void setOrderid(int orderid) {
+		this.orderid = orderid;
+	}
+
+	public String getPeriod() {
+		return this.period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public float getProfit() {
+		return this.profit;
+	}
+
+	public void setProfit(float profit) {
+		this.profit = profit;
+	}
+
+	public Date getStart() {
+		return this.start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Strategy getStrategy() {
+		return this.strategy;
+	}
+
+	public void setStrategy(Strategy strategy) {
+		this.strategy = strategy;
+	}
+
+}
---
+++ src/main/java/hu/fnf/devel/forex/database/Strategy.java
@@ -0,0 +1,61 @@
+package hu.fnf.devel.forex.database;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the Strategies database table.
+ * 
+ */
+@Entity
+@Table(name="Strategies")
+@NamedQuery(name="Strategy.findAll", query="SELECT s FROM Strategy s")
+public class Strategy implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(insertable=false, updatable=false)
+	private int id;
+
+	private String name;
+
+	//bi-directional many-to-one association to Order
+	@OneToMany(mappedBy="strategy")
+	private List<Order> orders;
+
+	public Strategy() {
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Order> getOrders() {
+		return this.orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+
+	public Order addOrder(Order order) {
+		getOrders().add(order);
+		order.setStrategy(this);
+
+		return order;
+	}
+
+	public Order removeOrder(Order order) {
+		getOrders().remove(order);
+		order.setStrategy(null);
+
+		return order;
+	}
+
+}
